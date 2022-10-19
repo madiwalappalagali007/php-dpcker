3e813072d2ac6acf05fde0a2a036b088b37362f0
Maddy-lagali <007maddylagali@gmail.com>
2022-10-19 14:30:04 +0530

    new

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 <title>Embed PHP in a .html File</title>
 </head>
 <body>
-<h1><?php echo "Hello World hu comment  hi newtry " ?></h1>
+<h1><?php echo "Hello World hu comment  hi newtry hi " ?></h1>
 </body>
 </html>
