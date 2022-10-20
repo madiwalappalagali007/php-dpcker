0feff53b62b1e36a1520f4c3142da7d28c776b43
Maddy-lagali <53235402+madiwalappalagali007@users.noreply.github.com>
2022-10-20 12:25:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<!-- <!DOCTYPE html>
-<html>
-<head>
-<title>Embed PHP in a .html File</title>
-</head>
-<body>
-<h1><?php echo "Hello World newtry " ?></h1>
-</body>
-</html>
-<!DOCTYPE html> -->
+
 <html>
 	<head>
 		<title>Calculator</title>
